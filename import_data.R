c19f8f3247ad2dd8643cdbeedce162f263217aac
KhawlaSeddiki <37381346+KhawlaSeddiki@users.noreply.github.com>
2021-05-07 08:28:34 -0400

    Update import_data.R

--- import_data.R
+++ import_data.R
@@ -11,3 +11,18 @@ import_sp <- function(spectra_files) {
   # Bin the intensities values according to the bin size
   bined_sp <- do.call(rbind, MSnbase::intensity(bin(sp_data, binSize=0.1)))
 }
+
+
+# binning function for intensity matrix (e.g. csv format)
+# min.mz : 1st column (usually equal to 1)
+# max.mz : number of columns
+# num.bin : number of bins
+binner <- function(spectra, min.mz=min.mz, max.mz=max.mz, num.bin=num.bin){
+  bin.width <-(max.mz - min.mz+1) / num.bin
+  binned <- numeric(num.bin)
+  for(i in 1:num.bin){
+    binned[i] <- sum(spectra[(min.mz+(i-1)*bin.width):(i*bin.width)])
+  }
+  return(binned)
+}
+binned_sp_matrix <- t(apply(sp_matrix, 1, binner))
